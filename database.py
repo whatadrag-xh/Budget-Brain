a9a856e6d5278e8d7d1c7db9d8a1c3dca42f2b29
whatadrag-xh <xuanhao0330@gmail.com>
2026-05-26 15:49:57 +0800

    Database functions done

--- database.py
+++ database.py
@@ -18,3 +18,34 @@ def init_db():
                 """)
         conn.commit()
     print("Database initialized successfully.")
+
+def add_transaction(date, description, amount, category, type):
+    with sqlite3.connect(DB_PATH) as conn:
+        try:
+            conn.execute("INSERT INTO transactions (date, description, amount, category, type) VALUES (?, ?, ?, ?, ?)", (date, description, amount, category, type))
+        except Exception as e:
+            print(f"Error: {e}")
+
+def get_all_transactions():
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.execute("SELECT * FROM transactions")
+        transaction_list = [dict(row) for row in cursor.fetchall()]
+        return transaction_list
+    
+def get_summary():
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.execute("""SELECT SUM(CASE WHEN type='income' THEN amount ELSE 0 END) AS total_income,
+                     SUM(CASE WHEN type='expense'THEN amount ELSE 0 END) AS total_expenses,
+                      SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) - SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS net FROM transactions""")
+        row = cursor.fetchone()
+        return dict(row)
+
+def delete_transaction(id):
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute("DELETE FROM transactions WHERE id = ?", (id,))
+
+if __name__ == "__main__":
+    init_db()
+
